5f0d2c6810771b0a8e85307ecc32290c49cd25a5
AYAN MUKHERJEE <116819600+Ayan1024@users.noreply.github.com>
2025-07-01 20:16:55 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU2C4hLQC0_ABdQj0jwjkYPHyshj93WKk",
-  authDomain: "mockly-b9500.firebaseapp.com",
-  projectId: "mockly-b9500",
-  storageBucket: "mockly-b9500.appspot.com",
-  messagingSenderId: "310497276144",
-  appId: "1:310497276144:web:9b70d7b586bb7cedb7d235",
-  measurementId: "G-H8LCWZCZEH"
+
 };
 
 const app = initializeApp(firebaseConfig);
